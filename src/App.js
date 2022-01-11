9a465910755c060dc5986763c148d67f873b0ec4
abdullahi0520 <90243393+abdullahi0520@users.noreply.github.com>
2022-01-11 17:32:58 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,40 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return(<div>
-      Github Card
-    </div>);
+    return(
+    <div>
+      <h1>Github Info</h1>
+      <form>
+      <input placeholder='Github Handle'/>
+      <button>Search</button>
+      </form>
+      <div id='UserCard'>
+      <img src='https://avatars.githubusercontent.com/u/90243393?v=4'/>
+      <a target='_blank' href='https://github.com/abdullahi0520'><h3>Abdullahi Ahmed</h3></a>
+      <p>(abdullahi0520)</p>
+      <p>Total Repos: 34</p>
+      <p>Total Following: 0</p>
+      </div>
+     <div id='following'>
+       <div className='following'>
+        <img width='200px' src='https://avatars.githubusercontent.com/u/11069106?v=4'/>
+        <a target='_blank' href='https://github.com/Divici'><p>Divici</p></a>
+       </div>
+       <div className='following'>
+        <img width='200px' src='https://avatars.githubusercontent.com/u/11069106?v=4'/>
+        <a target='_blank' href='https://github.com/Divici'><p>Divici</p></a>
+       </div>
+       <div className='following'>
+        <img width='200px' src='https://avatars.githubusercontent.com/u/11069106?v=4'/>
+        <a target='_blank' href='https://github.com/Divici'><p>Divici</p></a>
+       </div>
+       <div className='following'>
+        <img width='200px' src='https://avatars.githubusercontent.com/u/11069106?v=4'/>
+        <a target='_blank' href='https://github.com/Divici'><p>Divici</p></a>
+       </div>
+     </div>
+    </div>
+    );
   }
 }
 
